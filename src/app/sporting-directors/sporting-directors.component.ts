bdf7842adf150a9b1121f9e7470b13d792306bda
peggy4444 <rahimian.pegah@gmail.com>
2025-03-16 19:47:24 +0100

    deploy

--- src/app/sporting-directors/sporting-directors.component.ts
+++ src/app/sporting-directors/sporting-directors.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sporting-directors',
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './sporting-directors.component.html',
   styleUrl: './sporting-directors.component.css'
 })
-export class SportingDirectorsComponent {
 
+export class SportingDirectorsComponent {
+  constructor(private router: Router) {}
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 }
